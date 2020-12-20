29717864bccfd0e166b2466c2e84392378cb7d0e
Greg Stead <gregstead@gmail.com>
2020-12-20 15:39:55 -0600

    Fixed delete route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,19 @@
 // dependencies and data
 const router = require('express').Router();
 const fs = require('fs');
-const noteData = require('../db/db.json'); //Cache note data as JSON
 const { v4: uuidv4 } = require('uuid'); // to make a unique ID for each post
 
 router
     .route('/api/notes')
     // GET Requests
     .get((req, res) => {
+        const noteData = require('../db/db.json'); //Cache note data as JSON
         res.json(noteData)
-        console.log(`API GET: Success`);
     })
     // POST Requests
     .post((req, res) => {
+        //Cache note data as JSON
+        const noteData = require('../db/db.json');
         // Add a unique identifier to request
         req.body.id = uuidv4();
         // Receive a note and save on the request body
@@ -21,22 +22,23 @@ router
         noteData.push(newNote);
         fs.writeFileSync('./db/db.json', JSON.stringify(noteData), (err) => {
             if (err) throw err;
-            console.log(`API POST: Sucess`);
         });
         // Return the new note to the client
-        res.json(noteData);
+        res.end();
     });
 
 // DELETE requests
 router
     .route('/api/notes/:id')
     .delete((req, res) => {
+        //Cache note data as JSON
+        const noteData = require('../db/db.json');
         //Record to be deleted
         const deleteId = req.params.id;
         // Remove post with matching record ID
-        const newNoteData = noteData.filter((note) => note.id != deleteId);
+        const newNoteData = noteData.filter((note) => note.id !== deleteId);
         // Write changes to db
-        fs.writeFileSync('./db/db.json', JSON.stringify(newNoteData), (err) => {
+        fs.writeFile('./db/db.json', JSON.stringify(newNoteData), (err) => {
             if (err) throw err;
         });
         res.end();
