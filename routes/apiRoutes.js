34fc550be25ba7963fcff871a6d1ae71f268d4ec
Greg Stead <gregstead@gmail.com>
2020-12-20 22:01:17 -0600

    Refactored and fixed DELETE

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,15 +4,14 @@ const fs = require("fs");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid"); // to make a unique ID for each post
 
-// // Cache note data
-// const notesFile = path.join(__dirname, "../db/db.json");
+const dbPath = "../db/db.json";
 
 router
   .route("/api/notes")
   // GET Requests
   .get((req, res) => {
     // Send db as json
-    res.sendFile(path.join(__dirname, "../db/db.json"));
+    res.sendFile(path.join(__dirname, dbPath));
   })
   // POST Requests
   .post((req, res) => {
@@ -21,12 +20,12 @@ router
     // Receive a note and save on the request body
     const newNote = req.body;
     //Get the note data
-    fs.readFile(path.join(__dirname, "../db/db.json"), (err, data) => {
+    fs.readFile(path.join(__dirname, dbPath), (err, data) => {
       if (err) throw err;
       const noteData = JSON.parse(data);
       noteData.push(newNote);
       fs.writeFile(
-        path.join(__dirname, "../db/db.json"),
+        path.join(__dirname, dbPath),
         JSON.stringify(noteData),
         (err) => {
           if (err) throw err;
@@ -38,22 +37,26 @@ router
   });
 
 // DELETE requests
-// router.route("/api/notes/:id").delete(async (req, res) => {
-//   //Cache note data
-//   const noteData = await fs.readFile(notesFile, (err, data) => {
-//     if (err) throw err;
-//     return data;
-//   });
-//   //Record to be deleted
-//   const deleteId = req.params.id;
-//   // Remove post with matching record ID
-//   const newNoteData = {};
-//   // Write changes to db
-//   fs.writeFile(notesFile, JSON.stringify(newNoteData), (err) => {
-//     if (err) throw err;
-//   });
-//   console.log(`The file has been deleted`);
-//   res.end();
-// });
-
+router.delete("/api/notes/:id", (req, res) => {
+  // Get the ID from the request body
+  const deleteId = req.params.id;
+  // Open the database
+  fs.readFile(path.join(__dirname, dbPath), (err, data) => {
+    if (err) throw err;
+    // Parse the database data
+    const noteData = JSON.parse(data);
+    // Return all notes that don't have the deleteId
+    const newNoteData = noteData.filter((note) => note.id != deleteId);
+    // Write the new note data to the database
+    fs.writeFile(
+      path.join(__dirname, dbPath),
+      JSON.stringify(newNoteData),
+      (err) => {
+        if (err) throw err;
+        console.log(`The note was deleted`);
+      }
+    );
+  });
+  res.end();
+});
 module.exports = router;
